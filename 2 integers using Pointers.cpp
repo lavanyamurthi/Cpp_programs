ed1c918c85ec60f3e6114cf434f4ef90b0905bb0
lavanyamurthi <53431304+lavanyamurthi@users.noreply.github.com>
2020-06-29 11:05:41 +0530

    29 june 2020

---
+++ 2 integers using Pointers.cpp
@@ -0,0 +1,15 @@
+//Write a C++ program to find the sum of 2 integers using Pointers.
+#include <iostream> 
+using namespace std; 
+
+int main()
+{
+int f,s,*p,*q,sum;
+cout<<"Enter two integers to add\n";
+cin>>f>>s;
+p= &f;
+q=&s;
+sum= *p + *q;
+cout<<"Sum of 2 numbers="<< sum;
+return 0;
+}
